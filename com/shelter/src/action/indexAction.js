86abac3513305ee3181bd095862d80a9bc240d50
Unknown <1349363557@qq.com>
2020-03-02 18:19:38 +0800

    支持JOJO和新的游戏，以及以后的游戏
    新的更新，能够支持JOJO，或者新游戏，以及其他的游戏，只要传入gameId，根据此Id就可以切换策略了，也允许玩JOJO和CONNECTOR OMEGA游戏的人分开匹配不影响

--- com/shelter/src/action/indexAction.js
+++ com/shelter/src/action/indexAction.js
@@ -14,6 +14,11 @@ var num = 0;
 var socketPool = [];
 //匹配池：按照人数放置玩家的信息
 var matchPool = [];
+matchPool["jojo"] = [];
+matchPool["ConnectorOMEGA"] = [];
+
+var gameIdList = ["jojo", "ConnectorOMEGA"];
+
 //房间池：
 var roomPool = [];
 
@@ -22,7 +27,8 @@ var roomPool = [];
 module.exports = function (indexL) {
     index = indexL;
     for (var i = 0; i < 10; i++) {
-        matchPool[i] = [];
+        matchPool["jojo"][i] = [];
+        matchPool["ConnectorOMEGA"][i] = [];
     }
     init();
 }
@@ -87,21 +93,30 @@ var init = function () {
         //room消息广播
         socket.on('roomMsg', function (data) {
             if (data.room != null && data.room != "") {
+                //在现有的房间中查找房间号码一致的房间，得到序号一致的I
                 for (var i = 0; i < roomPool.length; i++) {
                     if (data.room == roomPool[i].room){
                         break;
                     }
                 }
-                if (data.msg.type == "down") {
-                    roomPool[i].keyData[roomPool[i].frame].push(data.msg);
-                } else if (data.msg.type == "up") {
-                    for (var j = 0; j < roomPool[i].keyData[roomPool[i].frame].length; j++) {
-                        if (roomPool[i].keyData[roomPool[i].frame][j].value == data.msg.value &&
-                            roomPool[i].keyData[roomPool[i].frame][j].playerId == data.msg.playerId) {
-                            roomPool[i].keyData[roomPool[i].frame].splice(j, 1);
-                            break;
+                if(i == roomPool.length){
+                    return;
+                }
+                if(roomPool[i].gameId == "jojo"){
+                    if (data.msg.type == "down") {
+                        roomPool[i].keyData[roomPool[i].frame].push(data.msg);
+                    } else if (data.msg.type == "up") {
+                        for (var j = 0; j < roomPool[i].keyData[roomPool[i].frame].length; j++) {
+                            if (roomPool[i].keyData[roomPool[i].frame][j].value == data.msg.value &&
+                                roomPool[i].keyData[roomPool[i].frame][j].playerId == data.msg.playerId) {
+                                roomPool[i].keyData[roomPool[i].frame].splice(j, 1);
+                                break;
+                            }
                         }
                     }
+                }
+                if(roomPool[i].gameId == "ConnectorOMEGA"){
+                    roomPool[i].keyData[roomPool[i].frame].push(data.msg);
                 }
             }
         });
@@ -139,7 +154,13 @@ var init = function () {
                 roomPool[i].keyData[roomPool[i].frame] = [];
                 
                 var room = roomPool[i];
-                roomPool[i].timer = setInterval(function () {gameStep(room)}, 100);
+
+                if(roomPool[i].gameId == "jojo"){
+                    roomPool[i].timer = setInterval(function () {gameStep(room)}, 100);
+                }
+                if(roomPool[i].gameId == "ConnectorOMEGA"){
+                    roomPool[i].timer = setInterval(function () {gameStep(room)}, 1000);
+                }
             }
         });
 
@@ -171,7 +192,12 @@ var init = function () {
             //即选用的人物，需要的房间总人数，token
             //上述信息以及将会被加入匹配队列，此外还有socket
             //playerNum指的是，玩家参与的匹配房间的总人数
-            matchPool[data.playerNum].push({
+            if(data.gameId == undefined || data.gameId == null){
+                var gameId = "jojo";
+            }else{
+                var gameId = data.gameId;
+            }
+            matchPool[gameId][data.playerNum].push({
                 "token": data.token,
                 "socket": socket,
                 //"playerId": data.playerId,
@@ -179,66 +205,69 @@ var init = function () {
                 //玩家匹配的人数
                 "playerNum": data.playerNum
             });
-            //加入了房间以后，如果大于了人数的话，将其取出，创建一个房间，并且加入到同一个房间中
-            while (matchPool[data.playerNum].length >= data.playerNum) {
-                //构造房间数据
-                var roomData = {
-                    "room": "room",
-                    "keyData": [],
-                    "socketPool": [],
-                    "playerData": [],
-                    //当前玩家人数
-                    "playerNum": 0,
-                    //预计玩家最大人数
-                    "maxPlayerNum": 0,
-                    //准备好的玩家的人数
-                    "readyPlayerNum": 0,
-                    "startFlag": false,
-                    "frame": 0,
-                    "timer": null
-                };
+            for(var gameName in gameIdList){
+                //加入了房间以后，如果大于了人数的话，将其取出，创建一个房间，并且加入到同一个房间中
+                while (matchPool[gameName][data.playerNum].length >= data.playerNum) {
+                    //构造房间数据
+                    var roomData = {
+                        "room": "room",
+                        "gameId": gameName,
+                        "keyData": [],
+                        "socketPool": [],
+                        "playerData": [],
+                        //当前玩家人数
+                        "playerNum": 0,
+                        //预计玩家最大人数
+                        "maxPlayerNum": 0,
+                        //准备好的玩家的人数
+                        "readyPlayerNum": 0,
+                        "startFlag": false,
+                        "frame": 0,
+                        "timer": null
+                    };
 
-                //playerData = {"playerRole","playerId","playerName","socket","connection"}
-                var num = data.playerNum;
-                roomData.maxPlayerNum = data.playerNum;
-                //使用for循环注入socket以及房间号
-                for (var i = 0; i < num; i++) {
-                    roomData.socketPool.push(matchPool[num][i].socket),
-                        roomData.playerData.push({
-                            "playerId": i,
-                            "playerName": "",
-                            "playerRole": matchPool[num][i].playerRole,
-                            "connection": true
+                    //playerData = {"playerRole","playerId","playerName","socket","connection"}
+                    var num = data.playerNum;
+                    roomData.maxPlayerNum = data.playerNum;
+                    //使用for循环注入socket以及房间号
+                    for (var i = 0; i < num; i++) {
+                        roomData.socketPool.push(matchPool[gameName][num][i].socket),
+                            roomData.playerData.push({
+                                "playerId": i,
+                                "playerName": "",
+                                "playerRole": matchPool[gameName][num][i].playerRole,
+                                "connection": true
+                            });
+                        roomData.room += '#' + matchPool[gameName][num][i].socket.id.slice(8, 15);
+                        roomData.playerNum++;
+                    }
+
+                    //将全部的socket加入房间中
+                    for (var i = 0; i < roomData.socketPool.length; i++) {
+                        //将其加入到房间中
+                        roomData.socketPool[i].join(roomData.room);
+                        //对其发送匹配完成的消息，包括房间号，所有玩家的数据，当前玩家分配到的玩家ID
+                        //这里发送了关于各种信息的消息，这样就可以通过这些信息建立各玩家的角色
+                        roomData.socketPool[i].emit('matchResult', {
+                            status: enumConfig.prototype.msgEnum.success,
+                            'room': roomData.room,
+                            'playerId': roomData.playerData[i].playerId,
+                            'playerData': roomData.playerData,
+                            'msg': 'ok'
                         });
-                    roomData.room += '#' + matchPool[num][i].socket.id.slice(8, 15);
-                    roomData.playerNum++;
+                    }
+                    // socket.to(roomData.room).emit('matchResult', {
+                    //     status:enumConfig.prototype.msgEnum.success, 
+                    //     'room': roomData.room,
+                    //     'playerId': roomData.playerData[i].playerId,
+                    //     'playerData':roomData.playerData,
+                    //     'msg':'ok'
+                    // });
+                    //将data加入到房间组中
+                    roomPool[roomPool.length] = roomData;
+                    //将匹配池中的玩家移除，从0开始，删除data.playerNum个
+                    matchPool[gameName][roomData.playerNum].splice(0, data.playerNum);
                 }
-
-                //将全部的socket加入房间中
-                for (var i = 0; i < roomData.socketPool.length; i++) {
-                    //将其加入到房间中
-                    roomData.socketPool[i].join(roomData.room);
-                    //对其发送匹配完成的消息，包括房间号，所有玩家的数据，当前玩家分配到的玩家ID
-                    //这里发送了关于各种信息的消息，这样就可以通过这些信息建立各玩家的角色
-                    roomData.socketPool[i].emit('matchResult', {
-                        status: enumConfig.prototype.msgEnum.success,
-                        'room': roomData.room,
-                        'playerId': roomData.playerData[i].playerId,
-                        'playerData': roomData.playerData,
-                        'msg': 'ok'
-                    });
-                }
-                // socket.to(roomData.room).emit('matchResult', {
-                //     status:enumConfig.prototype.msgEnum.success, 
-                //     'room': roomData.room,
-                //     'playerId': roomData.playerData[i].playerId,
-                //     'playerData':roomData.playerData,
-                //     'msg':'ok'
-                // });
-                //将data加入到房间组中
-                roomPool[roomPool.length] = roomData;
-                //将匹配池中的玩家移除，从0开始，删除data.playerNum个
-                matchPool[roomData.playerNum].splice(0, data.playerNum);
             }
         });
         //取消匹配的事件
@@ -246,10 +275,12 @@ var init = function () {
         //点击匹配的玩家如果点击了取消的话，将会触发
         //但是已经完成匹配的玩家。会因为上面的match返回值而无法匹配
         socket.on('cancelMatch', function (data) {
-            for (var player = 0; player < matchPool[data.playerNum].length; player++) {
-                if (matchPool[data.playerNum][player].socket.id == socket.id) {
-                    matchPool[data.playerNum].splice(player, 1);
-                    break;
+            for(var gameName in gameIdList){
+                for (var player = 0; player < matchPool[gameName][data.playerNum].length; player++) {
+                    if (matchPool[gameName][data.playerNum][player].socket.id == socket.id) {
+                        matchPool[gameName][data.playerNum].splice(player, 1);
+                        break;
+                    }
                 }
             }
         });
@@ -291,11 +322,13 @@ var init = function () {
             //断开后首先获取socket
             //首先从匹配池开始查找
             for (var i = 1; i < 10; i++) {
-                for (var j = 0; j < matchPool[i].length; j++) {
-                    //找到的话，将其移除
-                    if (matchPool[i][j].socket.id == socket.id) {
-                        matchPool[i].splice(j, 1);
-                        break;
+                for(var gameName in gameIdList){
+                    for (var j = 0; j < matchPool[gameName][i].length; j++) {
+                        //找到的话，将其移除
+                        if (matchPool[gameName][i][j].socket.id == socket.id) {
+                            matchPool[gameName][i].splice(j, 1);
+                            break;
+                        }
                     }
                 }
             }
